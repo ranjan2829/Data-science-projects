4131ec990518c29b9b693e047328031eeb4282cd
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-01-01 10:37:47 -0500

    Update Simple Stock prediction.py

--- Simple Stock prediction.py
+++ Simple Stock prediction.py
@@ -22,3 +22,37 @@ data = data[["Date", "Open", "High", "Low",
              "Close", "Adj Close", "Volume"]]
 data.reset_index(drop=True, inplace=True)
 print(data.head())
+figure = go.Figure(data=[go.Candlestick(x=data["Date"],
+                                        open=data["Open"], high=data["High"],
+                                        low=data["Low"], close=data["Close"])])
+figure.update_layout(title = "Google Stock Price Analysis", xaxis_rangeslider_visible=False)
+figure.show()
+figure = px.bar(data, x = "Date", y= "Close")
+figure.show()
+figure = px.line(data, x='Date', y='Close', 
+                 title='Stock Market Analysis with Rangeslider')
+figure.update_xaxes(rangeslider_visible=True)
+figure.show()
+figure = px.line(data, x='Date', y='Close', 
+                 title='Stock Market Analysis with Time Period Selectors')
+
+figure.update_xaxes(
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=3, label="3m", step="month", stepmode="backward"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+figure.show()
+figure = px.scatter(data, x='Date', y='Close', range_x=['2021-07-12', '2022-07-11'],
+                 title="Stock Market Analysis by Hiding Weekend Gaps")
+figure.update_xaxes(
+    rangebreaks=[
+        dict(bounds=["sat", "sun"])
+    ]
+)
+figure.show()
